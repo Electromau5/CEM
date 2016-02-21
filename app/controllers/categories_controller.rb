7926b192ea4968abd86d573cc97d4de87d6cb2bb
Pritish Sai <Pritish.sai@gmail.com>
2016-02-21 22:24:26 +0530

    Added 6 categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,26 @@ class CategoriesController < ApplicationController
 	def index
 		@category = Category.all
 	end
+
+	def new
+		@category = Category.new
+	end
+
+	def create
+		@category = Category.new(category_params)
+		if @category.save 
+			redirect_to category_path(@category)
+		end
+	end
+
+	def show
+		@divisions = Category.all
+		@category = Category.find(params[:id])
+	end
+
+private
+	def category_params
+		params.require(:category).permit(:name)
+	end
 end
+
