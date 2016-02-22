184e9e72ddd3489748c947afecd6f2aae5506ddf
Pritish Sai <Pritish.sai@gmail.com>
2016-02-22 12:19:21 +0530

    Changes to posts controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,8 @@
 class Post < ActiveRecord::Base
-	validates :title, presence: true, length: { minimum: 10, maximum: 100 }
-	validates :description, presence: true, length: { minimum: 100, maximum: 5000 }
-	has_attached_file :image, styles: { medium: ["300x300>", :jpg, :quality => 70], thumb: 
+	validates :title, :description, presence: true
+  	has_attached_file :image, styles: { medium: ["300x300>", :jpg, :quality => 70], thumb: 
   	                ["100x100>", :jpg, :quality => 70], large:  
   	                ['600>', :jpg, :quality => 70] }
-  	validates :image, attachment_presence: true
   	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   	belongs_to :category
 end
